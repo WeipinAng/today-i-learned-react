c6f1c55c610d90b6b8597704d013a82956222f94
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2024-08-23 15:27:26 +0800

    reveal API key thanks to the API being fully protected by Supabase's RLS (Row Level Security) policies

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://hahsrgaihmbnjabrjdci.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhaHNyZ2FpaG1ibmphYnJqZGNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQyMzMyODUsImV4cCI6MjAzOTgwOTI4NX0.5BgXiHf9pirGCC9ShR22735DWbaaCIEeZSkhtm50Tv4";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
