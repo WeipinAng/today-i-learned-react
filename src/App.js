ed8e343b2e86b2b6131a109004c8eecf88313dc4
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2024-08-23 09:59:15 +0800

    allow complete control of input field (controlled component) with dynamic word count

--- src/App.js
+++ src/App.js
@@ -86,22 +86,41 @@ const CATEGORIES = [
 ];
 
 function NewFactForm() {
+    const [text, setText] = useState("");
+    const [source, setSource] = useState("");
+    const [category, setCategory] = useState("");
+    const textLength = text.length;
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(text, source, category);
+    }
+
     return (
-        <form className="fact-form">
+        <form className="fact-form" onSubmit={handleSubmit}>
             <input
                 type="text"
                 name=""
                 id=""
                 placeholder="Share a fact with the world..."
+                value={text}
+                onChange={(e) => setText(e.target.value)}
             />
-            <span>200</span>
+            <span>{200 - textLength}</span>
             <input
                 type="text"
                 name=""
                 id=""
                 placeholder="Trustworthy source..."
+                value={source}
+                onChange={(e) => setSource(e.target.value)}
             />
-            <select name="" id="">
+            <select
+                name=""
+                id=""
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+            >
                 <option value="">Choose category:</option>
                 {CATEGORIES.map((cat) => (
                     <option key={cat.name} value={cat.name}>
