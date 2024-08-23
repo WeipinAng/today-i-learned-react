3d7c19c540d541d37e43bfb867d94967c76f2411
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2024-08-23 15:45:00 +0800

    load fact data from Supabase backend with useEffect into the app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import supabase from "./supabase";
 import "./style.css";
 
@@ -37,7 +37,17 @@ const initialFacts = [
 
 function App() {
     const [showForm, setShowForm] = useState(false);
-    const [facts, setFacts] = useState(initialFacts);
+    const [facts, setFacts] = useState([]);
+
+    useEffect(function () {
+        async function getFacts() {
+            const { data: facts, error } = await supabase
+                .from("facts")
+                .select("*");
+            setFacts(facts);
+        }
+        getFacts();
+    }, []);
 
     return (
         <>
